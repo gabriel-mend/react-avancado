5d26d4e8e37ace0acf0107777aa947477fcea251
Gabriel <gabrielmendoncapereira14@gmail.com>
2020-12-29 01:25:41 -0300

    Adding plop generator of components

---
+++ boilerplate/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as Styles from './styles';
+
+const Header = () => (
+    <Styles.Wrapper>
+        <h1>Header</h1>
+    </Styles.Wrapper>
+)
+
+export default Header;
---
+++ boilerplate/src/components/Header/stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import Header from '.';
+
+export default {
+    title: 'Header',
+    component: Header,
+} as Meta;
+
+export const Basic: Story = () => <Header />
